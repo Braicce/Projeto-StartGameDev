a56921c9189d860d7fb39b0f9620b75215b92ec8
Braice <62706067+Braicce@users.noreply.github.com>
2024-08-25 15:58:43 -0300

    V 1.0.3
    Add Actions
    
    Watering
    Farming
    
    Add HUD UI

---
+++ Assets/Scripts/HUD/HUDController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDController : MonoBehaviour
+{
+    [SerializeField] private Image waterUIBar;
+    [SerializeField] private Image woodUIBar;
+    [SerializeField] private Image carrotUIBar;
+
+     private PlayerItens playerItens;
+        private void Awake()
+    {
+        playerItens = FindObjectOfType<PlayerItens>();
+    }
+
+    void Start()
+    {
+        waterUIBar.fillAmount = 0f;
+        woodUIBar.fillAmount = 0f;
+        carrotUIBar.fillAmount = 0f;
+    }
+
+    void Update()
+    {
+        waterUIBar.fillAmount = playerItens.currentWater / playerItens.waterLimit;
+        woodUIBar.fillAmount = playerItens.totalWood / playerItens.woodLimit;
+        carrotUIBar.fillAmount = playerItens.totalCarrots / playerItens.carrotLimit;
+    }
+}
